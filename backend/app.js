dd84b3961f61b0c18a0612f1449ba533a9ee32ca
Josh Forman <jforman@cynerge.com>
2023-04-03 10:32:56 -0600

    update the stuff

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,35 @@ app.post('/signin', (req, res, next) => {
     message: 'Success',
     content: user
   })
+  // res.status(401).json({
+  //   message: 'Failed: poor login credentials',
+  // })
+})
+
+app.post('/googlesignin', (req, res, next) => {
+  // todo: log user in with email and pw returning a user object
+  const googleToken = req.body.gtoken
+  const user = {
+    id: 37289137,
+    name: "Robert Moses",
+    role: 'teacher',
+    googleToken: googleToken
+  }
+
+  // todo: get role definition of permissions
+  // attach permissions
+  user.permissions = {
+    screen1: true,
+    screen2: false
+  }
+
+  res.status(201).json({
+    message: 'Success',
+    content: user
+  })
+  // res.status(401).json({
+  //   message: 'Failed: poor login credentials',
+  // })
 })
 
 module.exports = app;
